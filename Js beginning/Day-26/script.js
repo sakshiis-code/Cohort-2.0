c4d6d9b69c79db307471329989057c95a04a185e
sakshiis-code <sakshihihisharma@gmail.com>
2025-10-25 13:42:21 +0530

    responsive, media queries

---
+++ Js beginning/Day-26/script.js
@@ -0,0 +1,17 @@
+// alert()
+
+var a = 12;
+let b = 12;
+const c = 12;
+
+a = 32;
+b = 64;
+c = 43; // will give an error
+
+console.log("heyyy");
+console.error("There is some bug");
+console.warn("Be safe");
+
+prompt("name");
+
+alert("hacked!");
