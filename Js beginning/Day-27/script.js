637ec3f4349efdca0822ee3714e65e8d27eac2e9
sakshiis-code <sakshihihisharma@gmail.com>
2025-10-26 09:16:51 +0530

    var let const | console | prompt alert | string | slice template-strings split replace replaceAll

---
+++ Js beginning/Day-27/script.js
@@ -0,0 +1,14 @@
+// var a = 12;
+let b = 33;
+const c = 24;
+
+console.log("heyheyeheey");
+console.info("abcdefghij");
+console.warn("abcdefghij");
+console.error("abcdefghij");
+
+// let nm = prompt("name?");
+// console.log(nm);
+
+// alert("something is wrong!");
+
